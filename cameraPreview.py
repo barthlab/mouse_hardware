689ef8b2fd36a14a2d28fc70e96fdefe4a062b18
Alex Kireeff <alexkireeff@gmail.com>
2023-05-09 11:21:10 -0400

    * trying a python script I found online

--- cameraPreview.py
+++ cameraPreview.py
@@ -1,11 +1,38 @@
 import picamera
-import time
+import  pyshine as ps #  pip3 install pyshine==0.0.9
+HTML="""
+<html>
+<head>
+<title>PyShine Live Streaming</title>
+</head>
 
-
-
-if "__main__" == __name__:
+<body>
+<center><h1> PyShine Live Streaming using PiCamera </h1></center>
+<center><img src="stream.mjpg" width='640' height='480' autoplay playsinline></center>
+</body>
+</html>
+"""
+def main():
+    StreamProps = ps.StreamProps
+    StreamProps.set_Page(StreamProps,HTML)
+    address = ('172.26.199.35',9000) # TODO
+    StreamProps.set_Mode(StreamProps,'picamera')    
     with picamera.PiCamera() as camera:
-        # TODO camera consistency?
         camera.resolution = (1024, 768)
-        camera.start_preview()
-        # TODO just hit ctrl+C to exit?
+        camera.framerate = 30
+
+        output = ps.StreamOut()
+        StreamProps.set_Output(StreamProps,output)
+        camera.start_recording(output, format='mjpeg')
+        try:
+            server = ps.Streamer(address, StreamProps)
+            print('Server started at','http://'+address[0]+':'+str(address[1]))
+            server.serve_forever()
+        finally:
+            camera.stop_recording()
+   
+        
+if __name__=='__main__':
+    main()
+    
+
