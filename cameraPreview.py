57d1683ca738706255d8a049cebf0026f189e8f4
Alex Kireeff <alexkireeff@gmail.com>
2023-05-10 14:34:41 -0400

    * we're doing the webserver way

--- cameraPreview.py
+++ cameraPreview.py
@@ -1,18 +1,98 @@
-import picamera
-import picamera.array
-import time
-import cv2
-from matplotlib import pyplot as plt
+# Web streaming example
+# Source code from the official PiCamera package
+# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
 
-with picamera.PiCamera() as camera:
-    cap=picamera.array.PiRGBArray(camera)
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    time.sleep(3)
-    camera.capture(cap,format="bgr")
-    img=cap.array
-    
-plt.figure().canvas.set_window_title("Hello Raspberry Pi")
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
+import io
+import picamera
+import logging
+import socketserver
+from threading import Condition
+from http import server
+
+PAGE="""\
+<html>
+<head>
+<title>Raspberry Pi - Surveillance Camera</title>
+</head>
+<body>
+<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
+<center><img src="stream.mjpg" width="640" height="480"></center>
+</body>
+/html>
+"""
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+ 
+ with picamera.PiCamera() as camera:
+     camera.resolution = (1024, 768)
+     output = StreamingOutput()
+     #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+     #camera.rotation = 90
+     camera.start_recording(output, format='mjpeg')
+     try:
+         address = ('172.26.199.35', 8000)
+         server = StreamingServer(address, StreamingHandler)
+         server.serve_forever()
+     finally:
+         camera.stop_recording()
+ 
+ # TODO simply the code
+ # TODO display only once?
