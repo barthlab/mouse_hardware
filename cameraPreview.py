c884a4b7dfc7c0d33ed73e3083da57d78ff08276
Alex Kireeff <alexkireeff@gmail.com>
2023-05-10 14:54:35 -0400

    * fixed last bug that I introduced

--- cameraPreview.py
+++ cameraPreview.py
@@ -55,6 +55,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
       self.send_header('Content-Type', 'text/html')
       self.send_header('Content-Length', len(content))
       self.end_headers()
+      self.wfile.write(content)
     elif self.path == '/stream.mjpg':
       self.send_response(200)
       self.send_header('Age', 0)
