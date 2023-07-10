a2eaa0a1beb3cbf942c2611b9ea35054e3241b3e
Alex Kireeff <alexkireeff@gmail.com>
2023-07-10 02:06:17 -0400

    * cleaned up file a bit

--- two_photon/post_processing.py
+++ two_photon/post_processing.py
@@ -1,93 +1,139 @@
 # TODO convert wheel ticks to speed? here rather than in the code?
 
+
+
 import cv2
-import numpy as np
+
+
 
 # Constants for circle detection
-MIN_RADIUS = 10
-MAX_RADIUS = 1000
-CIRCLE_COLOR = 50
+MIN_RADIUS = 15
+MAX_RADIUS = 100
+DEBUG_CIRCLE_COLOR = 50
 OUTLINE_THICKNESS = 1
 
+
+
 # Global variables for rectangle selection
-start_point = None
-end_point = None
+rect = None
 rectangle_selected = False
 
-# Mouse callback function for rectangle selection
+
+
 def select_rectangle(event, x, y, flags, param):
-    global start_point, end_point, rectangle_selected
+    global rect, rectangle_selected
 
-    if event == cv2.EVENT_LBUTTONDOWN:
-        start_point = (x, y)
-        rectangle_selected = False
-    elif event == cv2.EVENT_LBUTTONUP:
-        end_point = (x, y)
-        rectangle_selected = True
+    if not rectangle_selected:
+        if event == cv2.EVENT_LBUTTONDOWN:
+            rect = (x, y)
+        elif event == cv2.EVENT_LBUTTONUP:
+            rect = (rect[0], rect[1], x, y)
+            rectangle_selected = True
 
-# Function to detect circle in the selected area
+
+
 def detect_circle(frame, rect):
     x1, y1, x2, y2 = rect
-    roi = frame[y1:y2, x1:x2]  # Extract the region of interest
+    # Extract the region of interest
+    roi = frame[y1:y2, x1:x2]
 
     contours, _ = cv2.findContours(roi, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        print(contour)
         area = cv2.contourArea(contour)
 
         (x, y), radius = cv2.minEnclosingCircle(contour)
-        center = (int(x) + x1, int(y) + y1)  # Calculate the center coordinates in the original frame
 
-        if area > MIN_RADIUS ** 2 * np.pi and area < MAX_RADIUS ** 2 * np.pi:
-            # Draw circle outline
-            cv2.circle(frame, center, int(radius), CIRCLE_COLOR, OUTLINE_THICKNESS)
+        # Calculate the center coordinates in the original frame
+        center = (int(x) + x1, int(y) + y1)
 
-            return int(radius)
+        if MIN_RADIUS < radius and radius < MAX_RADIUS:
+            return (center, radius)
+
+    return None, -1
 
 
-    return None
 
-# Load video file
-video_path = '../data/mouse_video_pupil_alex.h264'
-cap = cv2.VideoCapture(video_path)
+def process_capture(capture):
+    # Default
+    ret1 = False
 
-# Create window and set mouse callback
-cv2.namedWindow('Video')
-cv2.setMouseCallback('Video', select_rectangle)
+    # Grab the frame
+    ret0, frame = capture.read()
 
-# Loop through video frames
-while True:
-    ret, frame = cap.read()
+    # If frame successfully grabbed
+    if ret0:
+        # Process frame into grayscale
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    if not ret:
-        print("Error: Failed to read frame from the video")
-        break
+        # Process frame into binary threshold
+        ret1, frame = cv2.threshold(frame, 58, 255, cv2.THRESH_BINARY) # TODO move constants out
 
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret, frame_thresh = cv2.threshold(frame_gray, 60, 255, cv2.THRESH_BINARY)
+    return(ret0, ret1, frame)
 
-    if not ret:
-        print("Error: Failed to convert frame to grayscale and threshold")
-        break
 
-    # Draw selected rectangle if available
-    if start_point is not None and end_point is not None:
-        cv2.rectangle(frame, start_point, end_point, (0, 255, 0), 2)
 
-        # Detect circle in selected area
-        if rectangle_selected:
-            x1, y1 = start_point
-            x2, y2 = end_point
-            rect = (x1, y1, x2, y2)
-            radius = detect_circle(frame_thresh, rect)
-            if radius is not None:
-                print("Radius:", radius)
+def main():
+    # Input vars
+    debug = True
+    video_path = '../data/mouse_video_pupil_alex.h264'
 
-    cv2.imshow('Video', frame_thresh)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Load video file
+    cap = cv2.VideoCapture(video_path)
 
-# Release resources
-cap.release()
-cv2.destroyAllWindows()
+    # Get 100th frame to display preview
+    for _ in range(100):
+        ret0, ret1, frame = process_capture(cap)
+        if not (ret0 and ret1):
+            sys.exit("Error parsing video")
+
+    # Make preview window to get region of interest
+    cv2.namedWindow("Preview")
+    cv2.imshow("Preview", frame)
+    cv2.setMouseCallback("Preview", select_rectangle)
+
+    # Wait until we have our region of interest
+    while not rectangle_selected:
+        cv2.waitKey(1)
+
+    # Clear
+    cv2.destroyAllWindows()
+
+
+    # Because the old cap had the first 100 frames dropped
+    cap = cv2.VideoCapture(video_path)
+
+    # Loop through video frames
+    while True:
+        ret0, ret1, frame = process_capture(cap)
+
+        if not ret0:
+            print("Error: Failed to read frame from the video")
+            break
+
+        if not ret1:
+            print("Error: Failed to convert frame to grayscale and threshold")
+            break
+
+        # Draw selected rectangle
+        cv2.rectangle(frame, rect[0:2], rect[2:4], (0, 255, 0), 2)
+
+        center, radius = detect_circle(frame, rect)
+
+        if radius != -1:
+            cv2.circle(frame, center, int(radius), DEBUG_CIRCLE_COLOR, OUTLINE_THICKNESS)
+
+        if debug:
+            cv2.imshow('Video', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+    # Release resources
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+if "__main__" == __name__:
+    main()
