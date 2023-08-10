7bb0274bd522bb44cd6eb3e096dcd43da3ab6381
Alex Kireeff <alexkireeff@gmail.com>
2023-08-10 14:23:30 -0400

    * now saves ttl_pulses file

--- two_photon/data_collection.py
+++ two_photon/data_collection.py
@@ -105,90 +105,98 @@ def main():
 
     filename = input("what do you want to save the experiment as?\n")
 
-    with open(f"{SAVE_DIR}/puff_data_{filename}.csv", "w") as puff_data_file: # TODO make prefix variable
-        with open(f"{SAVE_DIR}/distance_data_{filename}.csv", "w") as distance_data_file: # TODO make prefix variable
-            with open(f"{SAVE_DIR}/lick_data_{filename}.csv", "w") as lick_data_file: # TODO make prefix variable
+    with open(f"{SAVE_DIR}/ttl_data_{filename}.csv", "w") as ttl_data_file: # TODO make prefix variable
+        with open(f"{SAVE_DIR}/puff_data_{filename}.csv", "w") as puff_data_file: # TODO make prefix variable
+            with open(f"{SAVE_DIR}/distance_data_{filename}.csv", "w") as distance_data_file: # TODO make prefix variable
+                with open(f"{SAVE_DIR}/lick_data_{filename}.csv", "w") as lick_data_file: # TODO make prefix variable
 
-                puff_writer = csv.writer(puff_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                distance_writer = csv.writer(distance_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                lick_writer = csv.writer(lick_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    ttl_writer = csv.writer(ttl_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    puff_writer = csv.writer(puff_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    distance_writer = csv.writer(distance_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    lick_writer = csv.writer(lick_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-                with PiCameraRecordingContextManager(f"{SAVE_DIR}/mouse_video_{filename}.h264") as camera:
-                    # Send a short pulse
-                    GPIO.output(constants.VIDEO_TTL_PULSE, GPIO.HIGH)
-                    GPIO.output(constants.VIDEO_TTL_PULSE, GPIO.LOW)
+                    with PiCameraRecordingContextManager(f"{SAVE_DIR}/mouse_video_{filename}.h264") as camera:
+                        # Send a short pulse
+                        ttl = nano_to_milli(time.monotonic_ns())
+                        GPIO.output(constants.VIDEO_TTL_PULSE, GPIO.HIGH)
+                        GPIO.output(constants.VIDEO_TTL_PULSE, GPIO.LOW)
+                        ttl_writer.writerow(["video", ttl])
 
-                    time.sleep(initial_delay)
+                        time.sleep(initial_delay)
 
-                    for train_counter in range(num_trains_in_trial):
-                        for puff_counter in range(num_puffs_in_train):
+                        for train_counter in range(num_trains_in_trial):
+                            for puff_counter in range(num_puffs_in_train):
 
-                            # Check to see if still recording video
-                            camera.wait_recording(0)
+                                # Check to see if still recording video
+                                camera.wait_recording(0)
 
-                            # Randomly choose whether to use a real solenoid or a fake one
-                            puff_string = "fake"
-                            tmp_solenoid_pin = constants.FAKE_SOLENOID_PIN
-                            tmp_water_pin = constants.FAKE_SOLENOID_PIN
-                            if (random.random() * 100 > water_prob):
-                                puff_string = "real"
-                                tmp_water_pin = constants.WATER_SOLENOID_PIN
-                                tmp_solenoid_pin = constants.AIRPUFF_SOLENOID_PIN
+                                # Randomly choose whether to use a real solenoid or a fake one
+                                puff_string = "fake"
+                                tmp_solenoid_pin = constants.FAKE_SOLENOID_PIN
+                                tmp_water_pin = constants.FAKE_SOLENOID_PIN
+                                if (random.random() * 100 > water_prob):
+                                    puff_string = "real"
+                                    tmp_water_pin = constants.WATER_SOLENOID_PIN
+                                    tmp_solenoid_pin = constants.AIRPUFF_SOLENOID_PIN
 
-                            print(puff_string)
+                                print(puff_string)
 
-                            # Air puff / fake air puff
-                            GPIO.output(tmp_solenoid_pin, GPIO.LOW)
-                            solenoid_on = nano_to_milli(time.monotonic_ns())
-                            GPIO.output(constants.AIRPUFF_TTL_PULSE, GPIO.HIGH)
-                            time.sleep(air_time)
-                            GPIO.output(tmp_solenoid_pin, GPIO.HIGH)
-                            solenoid_off = nano_to_milli(time.monotonic_ns())
-                            GPIO.output(constants.AIRPUFF_TTL_PULSE, GPIO.LOW)
+                                # Air puff / fake air puff
+                                solenoid_on = nano_to_milli(time.monotonic_ns())
+                                GPIO.output(tmp_solenoid_pin, GPIO.LOW)
+                                ttl = nano_to_milli(time.monotonic_ns())
+                                GPIO.output(constants.AIRPUFF_TTL_PULSE, GPIO.HIGH)
+                                time.sleep(air_time)
+                                GPIO.output(tmp_solenoid_pin, GPIO.HIGH)
+                                solenoid_off = nano_to_milli(time.monotonic_ns())
+                                GPIO.output(constants.AIRPUFF_TTL_PULSE, GPIO.LOW)
 
-                            time.sleep(air_puff_to_water_release_time)
+                                time.sleep(air_puff_to_water_release_time)
 
-                            # Water release / fake water release
-                            GPIO.output(tmp_water_pin, GPIO.LOW)
-                            water_on = nano_to_milli(time.monotonic_ns())
-                            time.sleep(water_time)
-                            GPIO.output(tmp_water_pin, GPIO.HIGH)
-                            water_off = nano_to_milli(time.monotonic_ns())
+                                # Water release / fake water release
+                                water_on = nano_to_milli(time.monotonic_ns())
+                                GPIO.output(tmp_water_pin, GPIO.LOW)
+                                time.sleep(water_time)
+                                GPIO.output(tmp_water_pin, GPIO.HIGH)
+                                water_off = nano_to_milli(time.monotonic_ns())
 
-                            time.sleep(inter_puff_delay)
+                                time.sleep(inter_puff_delay)
 
-                            # Save current distance_marker_times
-                            prev_distance_marker_times, distance_marker_times = distance_marker_times, []
+                                # Save current distance_marker_times
+                                prev_distance_marker_times, distance_marker_times = distance_marker_times, []
 
-                            # Save current lick_times
-                            prev_lick_times, lick_times = lick_times, []
+                                # Save current lick_times
+                                prev_lick_times, lick_times = lick_times, []
 
-                            # Save data
-                            for data in list(prev_distance_marker_times):
-                                distance_writer.writerow([data])
+                                # Save data
 
-                            for data in prev_lick_times:
-                                lick_writer.writerow([data])
+                                ttl_writer.writerow(["puff", ttl])
 
-                            puff_writer.writerow([puff_string, count, solenoid_on, solenoid_off, water_on, water_off])
+                                for data in list(prev_distance_marker_times):
+                                    distance_writer.writerow([data])
+
+                                for data in prev_lick_times:
+                                    lick_writer.writerow([data])
+
+                                puff_writer.writerow([puff_string, count, solenoid_on, solenoid_off, water_on, water_off])
 
 
-                            count += 1
+                                count += 1
 
-                    time.sleep(final_delay)
+                        time.sleep(final_delay)
 
-                    # Save current distance_marker_times
-                    prev_distance_marker_times, distance_marker_times = distance_marker_times, []
+                        # Save current distance_marker_times
+                        prev_distance_marker_times, distance_marker_times = distance_marker_times, []
 
-                    # Save current lick_times
-                    prev_lick_times, lick_times = lick_times, []
+                        # Save current lick_times
+                        prev_lick_times, lick_times = lick_times, []
 
-                    # Save data
-                    for data in list(prev_distance_marker_times):
-                        distance_writer.writerow(data)
+                        # Save data
+                        for data in list(prev_distance_marker_times):
+                            distance_writer.writerow(data)
 
-                    for data in prev_lick_times:
-                        lick_writer.writerow([data])
+                        for data in prev_lick_times:
+                            lick_writer.writerow([data])
 
 
 if "__main__" == __name__:
